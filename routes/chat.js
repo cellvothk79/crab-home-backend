a4dd0207817f4c3f86b7abe97f2a6601470f6f45
cellvothk79 <3301285884@qq.com>
2026-06-27 23:56:39 +0800

    Create chat.js

---
+++ routes/chat.js
@@ -0,0 +1,7 @@
+const { searchMemories, extractAndStore, formatMemoriesForPrompt } = require('../services/memory');
+
+module.exports = function(app, supabase) {
+    // 👇 把 server.js 里的【拉取模型列表】、【核心对话】、【记忆压缩】、【拆分回复】全剪切过来！
+    
+    
+};
